63aa912437f5082631c935ce0ec033147acb6838
blegat <benoit.legat@gmail.com>
2025-03-03 12:05:40 +0000

    Deploying to gh-pages from @ blegat/LINMA2710@e70cd2e1c95476d909ec8bdfce7a3b51a641f087 🚀

---
+++ Lectures/MyUtils/src/luxor.jl
@@ -0,0 +1,43 @@
+import Luxor, PlutoTeachingTools
+
+function CenteredBoundedBox(str)
+    xbearing, ybearing, width, height, xadvance, yadvance =
+        Luxor.textextents(str)
+    lcorner = Luxor.Point(xbearing - width/2, ybearing)
+    ocorner = Luxor.Point(lcorner.x + width, lcorner.y + height)
+    return Luxor.BoundingBox(lcorner, ocorner)
+end
+
+function boxed(str::AbstractString, p)
+    Luxor.translate(p)
+    Luxor.sethue("lightgrey")
+    Luxor.poly(CenteredBoundedBox(str) + 5, action = :stroke, close=true)
+    Luxor.sethue("black")
+    Luxor.text(str, Luxor.Point(0, 0), halign=:center)
+    #settext("<span font='26'>$str</span>", halign="center", markup=true)
+    Luxor.origin()
+end
+
+function save_image_from_url(url, html_attributes...; name = split(url, '/')[end], kws...)
+    path = joinpath("cache", name)
+    return PlutoTeachingTools.RobustLocalResource(url, path, html_attributes...), path
+end
+
+function image_from_url(args...; kws...)
+    r, _ = save_image_from_url(args...; kws...)
+    return r
+end
+
+function placeimage_from_url(url, pos; scale = 1.0, centered = true, kws...)
+    r, path = save_image_from_url(url; kws...)
+    if r.mime isa MIME"image/svg+xml"
+        img = Luxor.readsvg(path)
+    else
+        img = Luxor.readpng(path)
+    end
+    Luxor.gsave()
+    Luxor.scale(scale)
+    Luxor.placeimage(img, pos / scale; centered)
+    Luxor.grestore() # undo `Luxor.scale`
+    return
+end
