e88dc574bb3c81f34d5fdf04e7abf0863dbb4a0d
Benoît Legat <benoit.legat@gmail.com>
2025-05-19 14:58:24 +0200

    Try enabling present mode

--- pluto-slider-server/generate_pdf.jl
+++ pluto-slider-server/generate_pdf.jl
@@ -1,9 +1,20 @@
 import PlutoPDF
 
+const enable_present = """`
+	<script>
+    window.present()
+	</script>
+`"""
+
 function generate_pdfs(dir)
+    tmp, _ = mktemp()
     for filename in readdir(dir)
         if endswith(filename, ".jl")
-            PlutoPDF.pluto_to_pdf(joinpath(dir, filename))
+            PlutoPDF.pluto_to_pdf(joinpath(dir, filename); generate_html_options = (; preamble_html_js = enable_present, PlutoPDF.generate_html_default_options...))
+            script = joinpath(@__DIR__, "remove_blank.sh")
+            pdf = joinpath(dir, filename[1:end-2] * "pdf")
+            mv(pdf, tmp)
+            run(`sh $script $tmp $pdf`)
         end
     end
 end
