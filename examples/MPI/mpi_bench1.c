a34efa993ce787a5d32fa009ddf7738e4741d599
Charles VH <127240273+CharlesVanHees@users.noreply.github.com>
2025-05-26 19:25:13 +0200

    Update mpi_bench1.c
    To make the code coherent with the comment in the slide (about the accuracy of the bandwidth measurement), I think we should measure on sender's side?

--- examples/MPI/mpi_bench1.c
+++ examples/MPI/mpi_bench1.c
@@ -13,13 +13,13 @@ int main() {
   for(int size = 1; size <= (1<<20); size <<= 1){
     char* buf = malloc(size);
     if (procid == 0) {
+        double tic = MPI_Wtime();
         MPI_Send(buf, size, MPI_CHAR, procid + 1, tag++, comm);
+        double toc = MPI_Wtime();
+        printf("[%d] I have send %d B in %f sec\n", procid, size, (toc-tic));
     }
     else {
-      double tic = MPI_Wtime();
       MPI_Recv(buf, size, MPI_CHAR, procid - 1, tag++, comm, MPI_STATUS_IGNORE);
-      double toc = MPI_Wtime();
-      printf("[%d] I have received %d B in %f sec\n", procid, size, (toc-tic));
     }
   }
   //codesnippet end
