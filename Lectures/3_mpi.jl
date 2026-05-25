9ea1a2a5c1fdc009976755315a9c3c37c2ceaf2e
MathiasGernez <126918984+MathiasGernez@users.noreply.github.com>
2026-05-25 06:18:54 +0200

    Fix the argument of the sum for the bandwith

--- Lectures/3_mpi.jl
+++ Lectures/3_mpi.jl
@@ -644,7 +644,7 @@ or 0 otherwise. Given ``S, T \subseteq V``,
 ```math
 \begin{align}
 \text{Width} &\qquad &  w(S, T) & = |\{ (u, v) \in E \mid u \in S, v \in T \}|\\
-\text{Bandwidth} & & \texttt{bw}(S, T) & = \sum_{u\in S, v\in T} w(u,v)
+\text{Bandwidth} & & \texttt{bw}(S, T) & = \sum_{u\in S, v\in T} \texttt{bw}(u,v)
 \end{align}
 ```
 """
